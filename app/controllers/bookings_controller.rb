e21ee71b386a75f60ff25d3862f58be64e09c8d9
Gaetano Osur <165330888+osur-gaetano@users.noreply.github.com>
2024-08-30 11:10:03 +0300

    Add bookings controller

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,26 @@
+class BookingsController < ApplicationController
+  def new
+    @flight = Flight.find(params[:flight_id])
+    @booking = Booking.new(flight: @flight)
+    params[:num_tickets].to_i.times(@booking.passengers.build)
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to @booking, notice: "Booking successfully created"
+    else
+      render :new
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+
+  private
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:name, :email])
+  end
+end
