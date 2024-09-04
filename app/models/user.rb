f410307565287ee347d8f7e84f8aa5c2bea496a5
Gaetano Osur <165330888+osur-gaetano@users.noreply.github.com>
2024-09-04 23:26:52 +0300

    Add validations on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   belongs_to :booking
 
-  validates_presence_of :username
-  validates_presence_of :email
+  validates :username, presence: true
+  validates :email, presence: true
 end
