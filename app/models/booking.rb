18750f33d2ece2e6c47b56c1345ba688f8463fc4
Gaetano Osur <165330888+osur-gaetano@users.noreply.github.com>
2024-09-04 23:23:59 +0300

    Add validation to booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,6 @@ class Booking < ApplicationRecord
   has_many :passangers, class_name: "User", dependent: :destroy
 
   accepts_nested_attributes_for :passangers
+
+  validates :flight_id, presence: true
 end
